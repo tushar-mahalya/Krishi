c1c9ef6b2742910d88298233da9f4da52542b6cb
tushar-mahalya <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>
2023-04-11 23:07:36 +0000

    logger format update

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%d/%m/%Y_%H:%M:%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
