90f2fcd2d61c86d34de8c11f595de796c4cef47a
tushar-mahalya <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>
2023-04-13 12:00:11 +0000

    plot_history function updated

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from src.exception import CustomException
 
@@ -11,7 +13,6 @@ def get_path(plant_dir:str, dir_test:str):
     This function accepts plants directory name (str)  and directory type as 
     input and returns the path of training, testing or validation directory
     whichever chosen.
-    
     '''
     try:
         if dir_test == 'Test':
@@ -27,38 +28,41 @@ def get_path(plant_dir:str, dir_test:str):
         raise CustomException(e, sys)
 
     
-def get_data_info(data_dir_path:str):
-    '''
-    This Function accepts the path of data directory as input and returns the
-    dictionary having all the details about it.
+def plot_history(model_info_dict:dict, plant_name:str):
     
+    '''
+    This function plots the accuracy and loss metrics of training and validation data
+    using Model Information Dictionary (artifacts/model_info.json) saved during model training.
     '''
     try:
-        plant_dirs = list(os.listdir(data_dir_path))
-        plants_data = {}
-        for plant in plant_dirs:
-            data_split_path = {
-                'train': get_path(plant, 'Train'),
-                'test': get_path(plant, 'Test'),
-                'val': get_path(plant, 'Val')
-            }
-            
-            classes_lst = []
-            for cat in os.listdir(get_path(plant,'Train')):
-                classes_lst.append(cat)
-            
-            plants_data[plant] = {
-            	'data_split':data_split_path,
-                'classes':classes_lst
-            }
-            
-            os.makedirs('/home/studio-lab-user/Krishi/artifacts', exist_ok = True)
-            
-            with open("/home/studio-lab-user/Krishi/artifacts/dataset_info.json", "w") as outfile:
-                json.dump(plants_data, outfile)
-                
-        print('Successfully saved Dataset Information in \'artifacts/dataset_info.json\'')
+        history = model_info_dict[plant_name]['Training History']
+
+        # Create subplots for loss and accuracy
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
+
+        # Plot training and validation loss
+        sns.lineplot(x=range(1, len(history['loss'])+1), y=history['loss'], ax=ax1, label='Training Loss')
+        sns.lineplot(x=range(1, len(history['val_loss'])+1), y=history['val_loss'], ax=ax1, label='Validation Loss')
+        ax1.set_title('Model Loss')
+        ax1.set_xlabel('Epoch')
+        ax1.set_ylabel('Loss')
+        ax1.legend()
+        ax1.grid(True)
+
+        # Plot training and validation accuracy
+        sns.lineplot(x=range(1, len(history['accuracy'])+1), y=history['accuracy'], ax=ax2, label='Training Accuracy')
+        sns.lineplot(x=range(1, len(history['val_accuracy'])+1), y=history['val_accuracy'], ax=ax2, label='Validation Accuracy')
+        ax2.set_title('Model Accuracy')
+        ax2.set_xlabel('Epoch')
+        ax2.set_ylabel('Accuracy')
+        ax2.legend()
+        ax2.grid(True)
+
+        # Add an overall title to the plot
+        plt.suptitle(f'{plant_name} Model Training History', fontsize=16, fontweight='bold')
+
+        # Display the plot
+        plt.show()
         
-    
     except Exception as e:
         raise CustomException(e, sys)
