155ca361f7b480ea37f10d152c181b714c811165
tushar-mahalya <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>
2023-04-18 01:23:24 +0000

    Added new plot_loss_accuracy() function

--- src/utils.py
+++ src/utils.py
@@ -31,16 +31,14 @@ def get_path(plant_dir:str, dir_test:str):
 def plot_history(model_info_dict:dict, plant_name:str):
     
     '''
-    This function plots the accuracy and loss metrics of training and validation data
+    This function plots the accuracy and loss fluctuation of training and validation data per epoch
     using Model Information Dictionary (artifacts/model_info.json) saved during model training.
     '''
     try:
         history = model_info_dict[plant_name]['Training History']
 
-        # Create subplots for loss and accuracy
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
 
-        # Plot training and validation loss
         sns.lineplot(x=range(1, len(history['loss'])+1), y=history['loss'], ax=ax1, label='Training Loss')
         sns.lineplot(x=range(1, len(history['val_loss'])+1), y=history['val_loss'], ax=ax1, label='Validation Loss')
         ax1.set_title('Model Loss')
@@ -49,7 +47,6 @@ def plot_history(model_info_dict:dict, plant_name:str):
         ax1.legend()
         ax1.grid(True)
 
-        # Plot training and validation accuracy
         sns.lineplot(x=range(1, len(history['accuracy'])+1), y=history['accuracy'], ax=ax2, label='Training Accuracy')
         sns.lineplot(x=range(1, len(history['val_accuracy'])+1), y=history['val_accuracy'], ax=ax2, label='Validation Accuracy')
         ax2.set_title('Model Accuracy')
@@ -58,10 +55,50 @@ def plot_history(model_info_dict:dict, plant_name:str):
         ax2.legend()
         ax2.grid(True)
 
-        # Add an overall title to the plot
         plt.suptitle(f'{plant_name} Model Training History', fontsize=16, fontweight='bold')
+        plt.show()
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+        
 
-        # Display the plot
+def plot_loss_accuracy(model_info_dict:dict, plant_name:str):
+    
+    '''
+    This function plots the accuracy and loss metrics of training, testing and validation data
+    using Model Information Dictionary (artifacts/model_info.json) saved during model training.
+    '''
+    try:
+        # define the data
+        loss = [round(val, 3) for val in model_info_dict[plant_name]['Loss Metrics'].values()]
+        acc = [round(val, 3) for val in model_info_dict[plant_name]['Accuracy Metrics'].values()]
+        data_split = ['Train', 'Test', 'Val']
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
+
+        ax1.bar(data_split, loss, color = 'red', zorder=2)
+        ax1.set_xticks(data_split)
+        ax1.set_xticklabels(data_split)
+        ax1.set_ylabel('Loss')
+        ax1.set_xlabel('Dataset Splits')
+        ax1.set_title('Loss metrics of all dataset splits')
+        ax1.grid(True)
+
+        for i in range(len(data_split)):
+            ax1.text(i,loss[i], loss[i], ha = 'center', bbox = dict(facecolor = 'cyan', alpha =0.8))
+
+        ax2.bar(data_split, acc, color = 'green', zorder=2)
+        ax2.set_xticks(data_split)
+        ax2.set_xticklabels(data_split)
+        ax2.set_ylabel('Accuracy')
+        ax2.set_xlabel('Dataset Splits')
+        ax2.set_title('Accuracy metrics of all dataset splits')
+        ax2.grid(True)
+
+        for i in range(len(data_split)):
+            ax2.text(i,acc[i],acc[i], ha = 'center', bbox = dict(facecolor = 'cyan', alpha =0.8))
+
+        plt.tight_layout()
         plt.show()
         
     except Exception as e:
